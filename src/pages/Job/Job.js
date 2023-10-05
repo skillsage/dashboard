e8112195086278b8195b299263ee4dee72f15840
Nathan Nunana Kulewoshie <55038849+NathanNunana@users.noreply.github.com>
2023-10-05 10:10:45 +0000

    last commit

--- src/pages/Job/Job.js
+++ src/pages/Job/Job.js
@@ -66,6 +66,82 @@ const Jobs = () => {
   const [expandedRows, setExpandedRows] = useState([]);
   const [loading, setLoading] = useState(true);
   const [api, contextHolder] = notification.useNotification();
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+  confirm();
+  setSearchText(selectedKeys[0]);
+  setSearchedColumn(dataIndex);
+};
+
+const handleReset = (clearFilters) => {
+  clearFilters();
+  setSearchText("");
+};
+
+const getColumnSearchProps = (dataIndex, columnTitle) => ({
+  filterDropdown: ({
+    setSelectedKeys,
+    selectedKeys,
+    confirm,
+    clearFilters,
+  }) => (
+    <div style={{ padding: 8 }}>
+      <Input
+        placeholder={`Search ${columnTitle}`}
+        value={selectedKeys[0]}
+        onChange={(e) =>
+          setSelectedKeys(e.target.value ? [e.target.value] : [])
+        }
+        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+        style={{ width: 188, marginBottom: 8, display: "block" }}
+      />
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </Space>
+    </div>
+  ),
+  filterIcon: (filtered) => (
+    <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+  ),
+  onFilter: (value, record) =>
+    record[dataIndex]
+      ? record[dataIndex]
+          .toString()
+          .toLowerCase()
+          .includes(value.toLowerCase())
+      : "",
+  onFilterDropdownVisibleChange: (visible) => {
+    if (visible) {
+      setTimeout(() => searchInput.select(), 100);
+    }
+  },
+  render: (text) =>
+    searchedColumn === dataIndex ? (
+      <Highlighter
+        highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+        searchWords={[searchText]}
+        autoEscape
+        textToHighlight={text ? text.toString() : ""}
+      />
+    ) : (
+      text
+    ),
+});
+
 
   const openNotification = (type, message, description) => {
     api[type]({
